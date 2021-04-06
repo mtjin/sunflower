5d00cffe9b0b1db7d672d576558c2bd6254f8bd9
jackjackE <tmddjs210@naver.com>
2021-04-06 22:37:11 +0900

    GardenActivity.kt 학습

--- app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
@@ -22,6 +22,16 @@ import androidx.databinding.DataBindingUtil.setContentView
 import com.google.samples.apps.sunflower.databinding.ActivityGardenBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+/* 출처 : https://developer.android.com/training/dependency-injection/hilt-android?hl=ko#android-classes
+* Application 클래스에 Hilt를 설정하고 애플리케이션 수준 구성요소를 사용할 수 있게 되면
+* Hilt는 @AndroidEntryPoint 주석이 있는 다른 Android 클래스에 종속 항목을 제공할 수 있습니다.
+*
+* Android 클래스에 @AndroidEntryPoint로 주석을 지정하면 이 클래스에 종속된 Android 클래스에도 주석을 지정해야 합니다.
+* 예를 들어 프래그먼트에 주석을 지정하면 이 프래그먼트를 사용하는 활동에도 주석을 지정해야 합니다.
+*
+* @AndroidEntryPoint는 프로젝트의 각 Android 클래스에 관한 개별 Hilt 구성요소를 생성합니다.
+* 이러한 구성요소는 구성요소 계층 구조에 설명된 대로 각 상위 클래스에서 종속 항목을 받을 수 있습니다.
+* */
 @AndroidEntryPoint
 class GardenActivity : AppCompatActivity() {
 
